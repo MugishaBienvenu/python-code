fb7bca2a4897b816a734eb4eb493e720ec79c3e5
MugishaBienvenu <mugishajeanbienvenu123@gmail.com>
2025-04-24 14:39:24 +0200

    triangle

--- triangle.py
+++ triangle.py
@@ -2,4 +2,30 @@ def pyramid(x):
     
   for i in range(x,5,1):
       print("*"*i)
-pyramid(0)      
+pyramid(0)
+
+def pyramid(x):
+    
+  for i in range(x,0,-1):
+      print("*"*i)
+pyramid(5)
+
+
+
+def triangle(a):
+    for i in range(0,a+1):
+        print(" "*(a-i)+"*"*(2*a-1))
+triangle(5)  
+
+
+
+def triangle(a):
+    for i in range(0,a+1):
+        print(" "*(a-i)+"*"*(2*i-1))
+triangle(5)   
+
+
+
+   
+       
+    
