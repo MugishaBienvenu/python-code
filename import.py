74f7e5bb5fab95814460e435b2c61f8c0c0aafda
MugishaBienvenu <mugishajeanbienvenu123@gmail.com>
2025-04-24 18:43:50 +0200

    module

--- import.py
+++ import.py
@@ -1,3 +1,9 @@
 from math import*
+
+if __name__=="__main__":
+    print("there is no languages are soft like python")
 print(int(sqrt(64)))
 print(factorial(5))
+
+x = lambda x: x*x
+print(x(1,3))
---
+++ module.py
@@ -0,0 +1,3 @@
+import calc
+print(calc.add(2,4))
+print(calc.subtract(2,4))
