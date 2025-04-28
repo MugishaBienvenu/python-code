164ca03573deaee0948d354e133eca90f4415cd3
MugishaBienvenu <mugishajeanbienvenu123@gmail.com>
2025-04-28 20:54:03 +0200

    swap

--- simple.py
+++ simple.py
@@ -1,46 +1,41 @@
-def add(x,y):
-    return x+y
-
-def subtract(x,y):
-    return x-y
-
-def multiply(x,y):
-    return x*y
-
-def division(x,y):
-    return x/y
-
-def division(x,y):
-    return x%y
-
-print("Choose any operator you want to use :")
-print("1.addition")
-print("2.subtract")
-print("3.multiply")
-print("4.division")
-print("5.modules")
-operator =int(input("select operator you chioce from 1 to 5:"))
-if operator == 6:
-    print("invalid operator")
-
-x =int(input("Enter number : "))
-y =int(input("Enter number : "))
-
-if operator == 1:
-    print(x+y)
+try:
     
-elif operator == 2:
-    print(x-y)
-    
-elif operator == 3:
-    print(x*y)
-    
-elif operator == 4:
-    print(x/y)
-    
-elif operator == 5:
-    print(x%y)
+    def new_func():
+        print("Choose any operator you want to use :")
+        print("1.addition")
+        print("2.subtract")
+        print("3.multiply")
+        print("4.division")
+        print("5.modules")
+
+        operator =int(input("select operator you chioce from 1 to 5:"))
+
+        if operator == 6:
+            print("exit")
+
+        x =float(input("Enter number : "))
+        y =float(input("Enter number : "))
+
+        if operator == 1:
+            print(x+y)
         
-    
-else :
-    print("there is no answer")                
+        elif operator == 2:
+            print(x-y)
+        
+        elif operator == 3:
+            print(x*y)
+        
+        elif operator == 4:
+            print(x/y)
+        
+        elif operator == 5:
+            print(x%y)
+            
+        
+        else :
+            print("there is no answer")
+
+    new_func()
+
+except:
+    print("division of zero is Error ")                
---
+++ swap.py
@@ -0,0 +1,10 @@
+def swap(x,y):
+    print(f"Before swapping numbers: X is {x}, Y is {y}")
+
+    temp=x
+    x=y
+    y=temp
+
+    print(f"After swapping numbers: X is {x}, Y is {y}")
+    
+swap(5,2)    
